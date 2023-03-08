0bed258d570e6d01db2d14118a1747d72322a0d2
King133x <126192712+King133x@users.noreply.github.com>
2023-03-07 16:27:16 -0800

    pyodbc errors

--- hello_app/views.py
+++ hello_app/views.py
@@ -49,54 +49,6 @@ def get_data():
         # render the template with the data
     return render_template("data.html", data=data)
 
-
-
-#defines route to send to entry page
-@app.route("/add_entry", methods=["GET", "POST"])
-def add_entry():
-    message = ""
-    if request.method == "POST":
-        # get form data
-        an = request.form.get("an")
-        model = request.form.get("model")
-        sn = request.form.get("sn")
-        nom = request.form.get("nom")
-        location = request.form.get("location")
-        cal_date = request.form.get("cal_date")
-        due = request.form.get("due")
-        cycle = 1  # assume 1 year cycle
-        manufacture = request.form.get("manufacture")
-        procedure = request.form.get("procedure")
-        cert_note = request.form.get("cert_note")
-        tech_note = request.form.get("tech_note")
-        cost = request.form.get("cost")
-        standard = request.form.get("standard")
-        facility = request.form.get("facility")
-
-        # check if item already exists in the database
-        connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:jtilabview.database.windows.net,1433;Database=JTISQL;Uid=LAB;Pwd=450032923Aa!1;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-        connection = pyodbc.connect(connection_string)
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM MASTER WHERE AN = ?", an)
-        
-        if cursor.fetchone():
-            # item already exists, set message
-            message = "Item already exists in the database."
-        else:
-            # item does not exist, insert into database
-            cursor.execute("INSERT INTO MASTER ([AN],[Model],[SN],[NOM],[LOC],[CAL DATE],[DUE],[CYCLE],[MANUFACTURE],[PROC],[SPECIAL CAL],[NOTE],[COST],[STANDARD],[Facility]) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                            an, model, sn, nom, location, cal_date, due, cycle, manufacture, procedure, cert_note, tech_note, cost, standard, facility)
-            connection.commit()
-            message = "Item has been successfully added to the database."
-        connection.close()
-
-    # if GET request or item already exists, render the add entry template with message
-    return render_template("add_entry.html", message=message)
-
-
-
-
-''''
 #defines route to send to entry page
 @app.route("/add_entry", methods=["GET", "POST"])
 def add_entry():
@@ -141,7 +93,7 @@ def add_entry():
         #return redirect(url_for("home"))
         # if GET request, render the add entry template
     return render_template("add_entry.html")
-    '''
+
 #defines route to send to check page   
 @app.route('/check_entry', methods=['POST'])
 def check_entry():

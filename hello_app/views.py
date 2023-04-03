cc2d07dc7119a21809660626196222d5e4363642
King133x <126192712+King133x@users.noreply.github.com>
2023-04-03 15:07:25 -0700

    Merged search

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,9 +1,9 @@
 ##################################################################################################################################################################################################
-import datetime
+from datetime import datetime
 from flask import Flask, render_template, url_for, redirect, request, flash
 from hello_app import app
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import Form, StringField, DateField, DecimalField, IntegerField
 import pyodbc
 ##################################################################################################################################################################################################
 # Initialize PYODBC connection
@@ -11,125 +11,56 @@ connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:jtilabvie
 connection = pyodbc.connect(connection_string)
 cursor = connection.cursor()
 ##################################################################################################################################################################################################
-# create form class
-class FilterForm(FlaskForm):
-    id = StringField('ID')
-    AN = StringField('AN')
-    Model = StringField('Model')
-    SN = StringField('SN')
-    NOM = StringField('NOM')
-    LOC = StringField('LOC')
-    CAL_DATE = StringField('CAL DATE')
-    DUE = StringField('DUE')
-    CYCLE = StringField('CYCLE')
-    MANUFACTURE = StringField('MANUFACTURE')
-    PROC = StringField('PROC')
-    SPECIAL_CAL = StringField('SPECIAL CAL')
-    NOTE = StringField('NOTE')
-    COST = StringField('COST')
-    STANDARD = StringField('STANDARD')
-    Facility = StringField('Facility')
-    submit = SubmitField('Search')
-class EditRecordForm(FlaskForm):
-    AN = StringField('AN')
-    Model = StringField('Model')
-    SN = StringField('SN')
-    NOM = StringField('NOM')
-    LOC = StringField('LOC')
-    CAL_DATE = StringField('CAL DATE')
-    DUE = StringField('DUE')
-    CYCLE = StringField('CYCLE')
-    MANUFACTURE = StringField('MANUFACTURE')
-    PROC = StringField('PROC')
-    SPECIAL_CAL = StringField('SPECIAL CAL')
-    NOTE = StringField('NOTE')
-    COST = StringField('COST')
-    STANDARD = StringField('STANDARD')
-    Facility = StringField('Facility')
-    submit = SubmitField('Update')
-##################################################################################################################################################################################################
-# defines route to send to home page
+# defines route to send to home page ********handles erros in flash********
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
 ##################################################################################################################################################################################################
-# defines route to send to about page
+# defines route to send to about page ********need to add scope of cal (maybe table in SQl and check within range)********
 @app.route("/about/")
 def about():
     return render_template("about.html")
 ##################################################################################################################################################################################################
-# defines route to send to contact page
+# defines route to send to contact page ********sorta complete********
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
 ##################################################################################################################################################################################################
-# defines route to send to entry page
-@app.route("/add_entry", methods=["GET", "POST"])
-def add_entry():  # get form data and and create entry if none exist
-    if request.method == "POST":
-        an = request.form.get("an")
-        model = request.form.get("model")
-        sn = request.form.get("sn")
-        nom = request.form.get("nom")
-        location = request.form.get("location")
-        cal_date = request.form.get("cal_date")
-        due = request.form.get("due")
-        cycle = 1  # assume 1 year cycle
-        manufacture = request.form.get("manufacture")
-        procedure = request.form.get("procedure")
-        cert_note = request.form.get("cert_note")
-        tech_note = request.form.get("tech_note")
-        cost = request.form.get("cost")
-        standard = request.form.get("standard")
-        facility = request.form.get("facility")
-        # check if item already exists in the database
-        cursor.execute(
-            "SELECT * FROM MASTER WHERE an = ? AND sn = ?", (an, sn))
-        if cursor.fetchone():
-            # item already exists, redirect to home page
-            flash("Item already exists!", "danger")  # show pop-up alert
-            return redirect(url_for("home"))
-        else:  # item does not exist, insert into database
-            cursor.execute("INSERT INTO MASTER ([AN],[Model],[SN],[NOM],[LOC],[CAL DATE],[DUE],[CYCLE],[MANUFACTURE],[PROC],[SPECIAL CAL],[NOTE],[COST],[STANDARD],[Facility]) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                           an, model, sn, nom, location, cal_date, due, cycle, manufacture, procedure, cert_note, tech_note, cost, standard, facility)
-            connection.commit()
-            flash("New entry added successfully!",
-                  "success")  # show pop-up alert
-            # redirect to home page
-            return redirect(url_for("home"))
-    else:  # if GET request, render the add entry template
-        return render_template("add_entry.html")
-
- # Render add entry form template with prefilled variables
-    return render_template("add_entry.html", an=an, model=model, sn=sn, nom=nom, location=location,
-                           cal_date=cal_date, due=due, manufacture=manufacture, procedure=procedure,
-                           cert_note=cert_note, tech_note=tech_note, cost=cost, standard=standard,
-                           facility=facility)
-##################################################################################################################################################################################################
-# defines route to send to test input
-@app.route('/test_request', methods=['GET', 'POST'])
-def test_request():
+@app.route('/master_search', methods=['GET', 'POST'])
+def master_search():
     if request.method == 'POST':
         # Get the AN and SN entered by user
         an = request.form['an']
         sn = request.form['sn']
-        cursor.execute(
-            'SELECT ID FROM MASTER WHERE AN = ? AND SN = ?', (an, sn))
-        master_id = cursor.fetchone()
-        if master_id:
-            # if MasterID exists, render test result page with MasterID passed as parameter
-            master_id = master_id[0]
-            return redirect(url_for('test_results', master_id=master_id))
-        else:  # if MasterID does not exist, render home page with error message passed as parameter
-            flash("No test results found!", "danger")  # show pop-up alert
-        return render_template('home.html',)
-    else:  # if GET request, render test request
-        return render_template('test_request.html')
+        if 'TID_search' in request.form:  # if Test button clicked
+            cursor.execute(
+                'SELECT ID FROM MASTER WHERE AN = ? AND SN = ?', (an, sn))
+            master_id = cursor.fetchone()
+            if master_id:
+                # if MasterID exists, render test result page with MasterID passed as parameter
+                master_id = master_id[0]
+                return redirect(url_for('test_results', master_id=master_id))
+            else:  # if MasterID does not exist, render home page with error message passed as parameter
+                flash("No test results found!", "danger")  # show pop-up alert
+                return render_template('home.html',)
+        elif 'MID_search' in request.form:  # if Data button clicked
+            cursor.execute(
+                'SELECT ID FROM MASTER WHERE AN = ? AND SN = ?', (an, sn))
+            master_id = cursor.fetchone()
+            if master_id:
+                # if MasterID exists, render data result page with MasterID passed as parameter
+                master_id = master_id[0]
+                return redirect(url_for('data_results', master_id=master_id))
+            else:  # if MasterID does not exist, render home page with error message passed as parameter
+                flash("No Equipment found!", "danger")  # show pop-up alert
+                return render_template('home.html',)
+    else:  # if GET request, render master search form
+        return render_template('master_search.html')
 ##################################################################################################################################################################################################
-# # defines route for test results page
+# defines route for test results page
 @app.route('/test_results/<int:master_id>', methods=['GET', 'POST'])
-def test_results(master_id):  # Get the test data entered by user or retrieve with Master_ID
+def test_results(master_id):  # add the test data entered by user or retrieve with Master_ID
     if request.method == 'POST':
         unit = request.form['unit']
         nom = request.form['nom']
@@ -149,46 +80,53 @@ def test_results(master_id):  # Get the test data entered by user or retrieve wi
         results = [dict(zip([column[0] for column in cursor.description], row))
                    for row in results]
         return render_template('test_results.html', results=results, master_id=master_id)
-    else:  # if test results do not exist, render test results page with message passed as parameter
-        flash("get test results dont exist", "danger")  # show pop-up alert
-        return render_template('home.html',)
+    else:  # if test results do not exist, render home results page with message passed as parameter
+        flash("test results dont exist", "danger")  # show pop-up alert
+        return render_template('home.html',)  
 ##################################################################################################################################################################################################
-# Define route for data search
-@app.route('/data_search', methods=['GET', 'POST'])
-def data_search():
-    if request.method == 'POST':
-        # Get the ID entered by user
-        id = request.form['id']
+@app.route('/data_results/<int:master_id>', methods=['GET', 'POST'])
+def data_results(master_id): # add the data entered by user or retrieve with Master_ID
+    if request.method == "POST":
+        an = request.form.get("an")
+        model = request.form.get("model")
+        sn = request.form.get("sn")
+        nom = request.form.get("nom")
+        location = request.form.get("location")
+        cal_date = request.form.get("cal_date")
+        due = request.form.get("due")
+        cycle = 1  # assume 1 year cycle
+        manufacture = request.form.get("manufacture")
+        procedure = request.form.get("procedure")
+        cert_note = request.form.get("cert_note")
+        tech_note = request.form.get("tech_note")
+        cost = request.form.get("cost")
+        standard = request.form.get("standard")
+        facility = request.form.get("facility")
+        # check if item already exists in the database
         cursor.execute(
-            'SELECT * FROM MASTER WHERE ID = ?', (id))
-        data = cursor.fetchone()
-        if data:  # if ID exists, render data result page with ID passed as parameter
-            data = data[0]
-            return redirect(url_for('data_results', data=data, id=id))
-        else:  # if ID does not exist, render home page with error message passed as parameter
-            flash("No test results found!", "danger")  # show pop-up alert
-            return render_template('home.html',)
-    else:  # if GET request, render test request
-        return render_template("data_search.html")
+            "SELECT * FROM MASTER WHERE AN = ? AND SN = ?", (an, sn))
+        if cursor.fetchone():
+            # item already exists, redirect to home page
+            flash("Item with AN & SN already exists", "danger")  # show pop-up alert
+            return redirect(url_for("home"))
+        else:  # item does not exist, insert into database
+            cursor.execute("INSERT INTO MASTER ([AN],[Model],[SN],[NOM],[LOC],[CAL DATE],[DUE],[CYCLE],[MANUFACTURE],[PROC],[SPECIAL CAL],[NOTE],[COST],[STANDARD],[Facility]) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                           (an, model, sn, nom, location, cal_date, due, cycle, manufacture, procedure, cert_note, tech_note, cost, standard, facility))
+            connection.commit()
+            flash("New entry added successfully!", "success")  # show pop-up alert
+            # redirect to home page
+            return redirect(url_for("home"))
+    else:  # request method is GET Retrieve the data results from the database
+        cursor.execute('SELECT * FROM MASTER WHERE ID = ?', (master_id,))
+        results = cursor.fetchall()
+        if results:  # if data results exist
+            results = [dict(zip([column[0] for column in cursor.description], row)) for row in results]
+            return render_template('data_results.html', results=results, master_id=master_id)
+        else:  # if data results do not exist
+            flash("No data results found!", "danger")
+            return redirect(url_for("home"))
 ##################################################################################################################################################################################################
-# defines route to send to data page
-@app.route("/data_results/<int:id>", methods=["GET", "POST"])
-def data_results(id):
-    if request.method == 'GET':
-        # Execute the SELECT statement to retrieve the data with the given ID
-        cursor.execute('SELECT * FROM MASTER WHERE ID = ?', (id,))
-        data = cursor.fetchone()
-
-    if data:  # if data results exist, render data results page with data and id passed as parameters
-        #data = [dict(zip([column[0] for column in cursor.description], row))
-                   #for row in data]
-        return render_template('data_results.html', data=data, id=id)
-    else:
-        # if test results do not exist, render message
-        flash("data results don't exist", "danger")  # show pop-up alert
-        return render_template('home.html', )
 ##################################################################################################################################################################################################
 # run app in debug mode
 if __name__ == '__main__':
     app.run(debug=True)
-##################################################################################################################################################################################################

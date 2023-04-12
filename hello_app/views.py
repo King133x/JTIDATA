88e2fdb9ba73708f3c77b4e32762013b162165e0
King133x <126192712+King133x@users.noreply.github.com>
2023-04-12 14:40:43 -0700

    DATATABLE IN!!

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,6 +1,6 @@
 ##################################################################################################################################################################################################
 from datetime import datetime
-from flask import Flask, render_template, url_for, redirect, request, flash
+from flask import Flask, render_template, url_for, redirect, request, flash, jsonify
 from hello_app import app
 from flask_wtf import FlaskForm
 from wtforms import Form, StringField, DateField, DecimalField, IntegerField
@@ -12,40 +12,45 @@ connection = pyodbc.connect(connection_string)
 cursor = connection.cursor()
 ##################################################################################################################################################################################################
 # defines route to send to home page ********handles erros in flash********
+
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
 ##################################################################################################################################################################################################
 # defines route to send to about page ********need to add scope of cal (maybe table in SQl and check within range)********
+
+
 @app.route("/about/")
 def about():
     return render_template("about.html")
 ##################################################################################################################################################################################################
 # defines route to send to contact page ********sorta complete********
+
+
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
 ##################################################################################################################################################################################################
-@app.route('/master_search/', methods=['GET', 'POST'])
-def master_search():
-    if request.method == 'POST': # Get the MID by user
-        master_id = request.form['master_id']
-        cursor.execute('SELECT * FROM MASTER WHERE ID = ?', (master_id,))
-        master_result = cursor.fetchone()
-        if master_result: # if MasterID exists, send MasterID to master_result page
-            return redirect(url_for('master_results', master_id=master_id,))
-        else:  # if MasterID does not exist, render home page with error message passed as parameter
-            flash("No equipment found with this MID!",
-                  "danger")  # show pop-up alert
-            return render_template('home.html',)
-    else:  # if GET request, render master search form
+# handles the data search ajax uses post to get data
+@app.route('/master_data/', methods=['GET', 'POST'])
+def master_data():
+    if request.method == 'POST':
         cursor.execute('SELECT * FROM MASTER')
         master_search = cursor.fetchall()
-        return render_template('master_search.html', master_search=master_search)
+        if master_search:
+            master_search = [dict(zip([column[0] for column in cursor.description], row)) for row in master_search]
+            return jsonify({"data": master_search})
+        else:
+            flash("No equipment found with this MID!", "danger")  # show pop-up alert
+            return redirect(url_for('home'))
+    else:
+        return render_template('master_search.html')
+
 ##################################################################################################################################################################################################
-@app.route('/master_results/<int:master_id>', methods=['GET', 'POST'])
-def master_results(master_id):
+@app.route('/master_record/<int:master_id>', methods=['GET', 'POST'])
+def master_record(master_id):
     if request.method == "POST":
         unit = request.form['unit']
         nom = request.form['nom']
@@ -55,45 +60,48 @@ def master_results(master_id):
                        (master_id, unit, nom, actual, tol))
         cursor.commit()
         flash("Test results added successfully!", "success")
-        return redirect(url_for("master_results", master_id=master_id))
+        return redirect(url_for("master_record", master_id=master_id))
     else:
         cursor.execute('SELECT * FROM MASTER WHERE ID = ?', (master_id,))
         master_results = cursor.fetchall()
-        if master_results: # show master results
-            master_results = [dict(zip([column[0] for column in cursor.description], row)) for row in master_results]
-            cursor.execute('SELECT * FROM TEST WHERE Master_ID = ?', (master_id,))
+        if master_results:  # show master results
+            master_results = [dict(
+                zip([column[0] for column in cursor.description], row)) for row in master_results]
+            cursor.execute(
+                'SELECT * FROM TEST WHERE Master_ID = ?', (master_id,))
             test_results = cursor.fetchall()
-            if test_results: # include test report if found
+            if test_results:  # include test report if found
                 test_results = [dict(zip([column[0] for column in cursor.description], row))
-                           for row in test_results]
+                                for row in test_results]
             else:
                 flash("No test results found!", "danger")
-            return render_template('master_results.html', master_results=master_results, master_id=master_id, test_results=test_results)
+            return render_template('master_record.html', master_results=master_results, master_id=master_id, test_results=test_results)
         else:
-            flash("No master results found!", "danger")
+            flash("No master record found!", "danger")
             return redirect(url_for("home"))
 ##################################################################################################################################################################################################
-@app.route('/master_edit/<int:master_id>', methods=['GET', 'POST'])
-def master_edit(master_id,):
-    field = request.form['field']
-    if request.method == 'GET':
-        # Fetch the current value of the field from the database
-        cursor.execute("SELECT {} FROM MASTER WHERE id = ?".format(field), (master_id,))
-        value = cursor.fetchone()[0]
-        
-        # Render a form with the current value of the field
-        return render_template('master_edit.html', field=field, value=value)
+# @app.route('/master_edit/<int:master_id>', methods=['GET', 'POST'])
+# def master_edit(master_id,):
+#     field = request.form['field']
+#     if request.method == 'GET':
+#         # Fetch the current value of the field from the database
+#         cursor.execute("SELECT {} FROM MASTER WHERE id = ?".format(field), (master_id,))
+#         value = cursor.fetchone()[0]
 
-    elif request.method == 'POST':
-        # Extract the new value of the field from the form submission
-        new_value = request.form.get('new_value')
-        
-        # Update the field in the database
-        cursor.execute("UPDATE MASTER SET {} = ? WHERE id = ?".format(field), (new_value, master_id))
-        connection.commit()
-        
-        # Redirect back to the previous page
-        return redirect(request.referrer)
+#         # Render a form with the current value of the field
+#         return render_template('master_edit.html', field=field, value=value)
+
+#     elif request.method == 'POST':
+#         # Extract the new value of the field from the form submission
+#         new_value = request.form.get('new_value')
+
+#         # Update the field in the database
+#         cursor.execute("UPDATE MASTER SET {} = ? WHERE id = ?".format(field), (new_value, master_id))
+#         connection.commit()
+
+#         # Redirect back to the previous page
+#         return redirect(request.referrer)
+##################################################################################################################################################################################################
 
 ##################################################################################################################################################################################################
 # run app in debug mode
